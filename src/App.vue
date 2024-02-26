6c91d897c03f08619a6401bc68b3b9ed24cd7d4e
ZinchAndrey <zinchandrey@gmail.com>
2024-02-26 23:11:14 +0300

    add WeatherCard.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
-
-
 <template>
   <main>
+    <weather-card/>
   </main>
 </template>
 
 <script>
+import WeatherCard from '@/components/WeatherCard.vue';
+
+export default {
+  components: {
+    WeatherCard,
+  }
+}
 </script>
 
 <style scoped>
+main {
+  min-height: 100vh;
+  width: 100%;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card">
+    <div class="card__top">
+      <span class="card__temp">29</span>
+      <span class="card__city">Moscow</span>
+    </div>
+    <div class="card__sub">
+      <span class="card__date">
+        Wednesday, 25 May
+      </span>
+      <div class="card__weather-block weather">
+        <span class="weather__type">
+          Mostly sunny
+        </span>
+        <span class="weather__temp">
+          25 / 30
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.card {
+  width: 400px;
+  min-height: 500px;
+  margin: 0 auto;
+
+  border-radius: 20px;
+  box-shadow: 0 0 10px 0px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+
+  &__top {
+    font-size: 30px;
+    line-height: 120%;
+    font-weight: 700;
+    color: var(--alt-text-color);
+
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    background-color: #5f5f5f;
+
+    min-height: 400px;
+  }
+
+  &__temp {
+    font-size: 40px;
+    margin-bottom: 10px;
+  }
+
+  &__sub {
+    color: var(--text-color);
+
+    display: flex;
+    flex-direction: column;
+    padding: 20px 30px;
+  }
+
+  &__date {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+}
+
+.weather {
+  font-weight: 600;
+  color: var(--sub-text-color);
+
+  display: flex;
+  flex-direction: column;
+
+  &__type {
+    margin-bottom: 5px;
+  }
+
+  // &__temp {}
+}
+</style>
